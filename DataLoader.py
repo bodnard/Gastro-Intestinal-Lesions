f8fa922a84769507cdbc358c9db1c1dd66187159
bodnard <60945886+bodnard@users.noreply.github.com>
2020-07-15 16:32:55 +0200

    update
    it gives back all the chosen video's given frames and for each frame the ground truth resection = "1", no-resection = "0"

--- DataLoader.py
+++ DataLoader.py
@@ -1,30 +1,61 @@
+# returns: [ (numpy.ndarray, str), ...]
+#          [ (frame of a vid, label), ...]
+# image shape: 576, 768, 3
 
-import cv2 as cv
+import cv2
+import time
 
-STEP = 5
+def DataLoader(what, datapath, step):
 
-def DataLoader(what, datapath):
-
+    STEP = step
+    directory = datapath
+    
     frames = []
+    f_labels = []  # the labels for each frame
+    frame_label_list = []
     f_counter = 0
 
     if what == "train":
-        video = cv.VideoCapture('data/train/adenoma_01_WL.mp4')
+        directory += "train/"
 
     if what == "val":
-        video = cv.VideoCapture('data/val/adenoma_10_WL.mp4')
+        directory += "val/"
+    
+    labels = open(directory + "labels.txt")
+    video_labels = labels.read().splitlines()
+    
+    names = open(directory + "names.txt")
+    video_names = names.read().splitlines()
+    # print(video_names)
+    
+        for index, video_name in enumerate(video_names):
+        video = cv2.VideoCapture(directory + video_name)
+        vid_set = len(video_names)
+        while True:
+            has_frame, frame = video.read()
 
-    while True:
-        has_frame, frame = video.read()
+            if not has_frame:
+                # print('Reached the end of ', index + 1, '/', vid_set, ' video')
+                print('fram shape:', frames[-1].shape)
+                # print('selected frames:', f_counter // STEP)
+                break
 
-        if not has_frame:
-            print('Reached the end of the video')
-            print('selected frames:', f_counter // STEP)
-            return frames
+            if f_counter % STEP == 0:
+                # cv2.imshow('frame', frame)
+                # key = cv2.waitKey(50)
+                frames.append(frame)
+                f_labels.append(video_labels[index])
+                frame_label_list.append((frames[-1], f_labels[-1]))
 
-        if f_counter % STEP == 0:
-            # cv2.imshow('frame', frame)
-            # key = cv2.waitKey(50)
-            frames.append(frame)
+            f_counter += 1
+    print("selected frames:", f_counter // STEP)
+    # cv2.destroyAllWindows()
+    return frame_label_list
 
-        f_counter += 1
+
+startTime = time.time()
+
+DataLoader("train", "data/", 10)
+
+executionTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(executionTime))
